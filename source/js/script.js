15e28bdab708fa4b76f208beb1dab78ade7e0275
Olga Peganova <volha25121987@gmail.com>
2023-06-14 20:27:46 +0700

    map marker

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,32 @@
-/* в этот файл добавляет скрипты*/
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+var map = L.map('map').setView([59.968318, 30.317224], 17.61);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var myIcon = L.icon({
+  iconUrl: 'img/images-map/marker-map.svg',
+  iconSize: [38, 50],
+  iconAnchor: [19, 50],
+  // popupAnchor: [-3, -76],
+});
+
+
+L.marker([59.968318, 30.317224], {icon: myIcon}).addTo(map);
+
